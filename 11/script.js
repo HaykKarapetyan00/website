3dcb06adedb73f2b53b073e83a82673d40820020
HaykKarapetyan00 <134404361+HaykKarapetyan00@users.noreply.github.com>
2023-11-23 17:26:54 +0400

    Add files via upload

---
+++ 11/script.js
@@ -0,0 +1,9 @@
+function visib() 
+{
+	if(	document.getElementById("drp_chng").style.visibility === "visible"){
+			document.getElementById("drp_chng").style.visibility = "hidden";
+	}
+	else{
+			document.getElementById("drp_chng").style.visibility = "visible";
+	}
+}
